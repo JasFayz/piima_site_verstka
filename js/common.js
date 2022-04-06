7509d39f7f9a75728f2a61be3d4e914bb087363d
Jasurjon M. Akhmatfaezov <j_akhmatfaezov@uzinfocom.local>
2022-04-06 18:20:37 +0500

    add other pages

--- js/common.js
+++ js/common.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	// $('body').hide()
-
+	$('.header__menu-btn').click(function(){
+		$(this).children().toggleClass('is-active')
+		$('.header__nav').toggleClass('show')
+	})
 })
